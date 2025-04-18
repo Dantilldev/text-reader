6e06aa59c0ffcd27e15b8fa45897af1a66e0cde5
Daniel Tilleria <daniel.tilleria@hotmail.com>
2025-04-18 18:10:35 +0200

    completed the project

--- script.js
+++ script.js
@@ -60,15 +60,88 @@ data.forEach(createBox);
 
 // skapar en box för varje objekt i data
 function createBox(item) {
-  box = document.createElement("div");
+  const box = document.createElement("div");
 
   const {image, text} = item; // destructing för image och text
 
   box.classList.add("box"); // box får en klass
 
   box.innerHTML = `
+
+  <div class="image-wrapper">
   <img src="${image}" alt="${text}" />
+  </div>
   <p class="info">${text}</p>`;
 
-  main.appendChild(box); // lägger till boxen i main
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+
+    // Lägger till active och tar bort den efter 800ms
+    box.classList.add("active");
+    setTimeout(() => box.classList.remove("active"), 800);
+  });
+
+  // lägger till boxen i main
+  main.appendChild(box);
 }
+
+// Sätter texten i message
+const message = new SpeechSynthesisUtterance();
+
+// Lägger till röster i en array
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  // Lägger till röster i select med en for-loop
+  for (let i = 0; i < voices.length; i++) {
+    const voice = voices[i];
+
+    const option = document.createElement("option");
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  }
+}
+// Sätter texten som ska läsas upp
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// Läs texten
+function speakText() {
+  speechSynthesis.cancel();
+  speechSynthesis.speak(message);
+}
+// Ändra rösten
+function setVoice(e) {
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+}
+
+// Ändra Röster
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+// Visa och dölja text-box
+toggleBtn.addEventListener("click", () => {
+  document.getElementById("text-box").classList.toggle("show");
+});
+
+// Stäng knappen
+closeBtn.addEventListener("click", () => {
+  document.getElementById("text-box").classList.remove("show");
+});
+
+// onchange event för select
+voicesSelect.addEventListener("change", setVoice);
+
+// Läs knappen
+readBtn.addEventListener("click", () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getVoices();
